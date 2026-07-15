a643e576b3979f6ef9748237be2d64fde3289123
Data.ag <data.ag@proton.me>
2026-07-15 15:33:57 +0200

    feat: add mrt_shipping_report with tests and docs

---
+++ models/mart/mrt_shipping_report.sql
@@ -0,0 +1,18 @@
+SELECT
+    order_id,
+    order_date,
+    required_date,
+    shipped_date,
+    store_id,
+    store_name,
+    staff_id,
+    staff_name,
+    DATE_DIFF(shipped_date, order_date, DAY) AS days_to_ship,
+    DATE_DIFF(shipped_date, required_date, DAY) AS days_late,
+    CASE
+        WHEN shipped_date IS NULL THEN 'To ship'
+        WHEN shipped_date > required_date THEN 'Late'
+        ELSE 'On time'
+    END AS shipping_status
+FROM {{ ref('int_local_bike_database__orders') }}
+WHERE order_status != 3
